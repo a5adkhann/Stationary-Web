e03c950b08ad97b1432c9dc3ebda975ab373d35a
Kinza Mushtaq <74036470+Kinza-Khan@users.noreply.github.com>
2024-12-24 07:39:41 -0800

    Cart Created

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let hamburgerIcon = document.getElementById("hamburger-icon");
 let mobileNavbar = document.getElementById("navbar-mobile-view");
+let cartIcon = document.getElementById("cart-icon");
+let cart = document.getElementById("cart");
+let closeIcon = document.getElementById("close-icon");
 
 
 hamburgerIcon.addEventListener("click", () => {
     if(mobileNavbar){
     mobileNavbar.classList.toggle("hide");
 }
+})
+
+cartIcon.addEventListener("click", () => {
+    if(cart) {
+        cart.classList.toggle("show");
+    }
+})
+
+closeIcon.addEventListener("click", () => {
+    cart.classList.toggle("show");
 })
